db489c6298e44f3de096cbbb029e008db21b1f9d
glqdlt <dlfdnd0725@gmail.com>
2019-01-18 18:18:31 +0900

    구현

--- src/main/java/com/glqdlt/ex/replicationjdbc/DataSourceConfig.java
+++ src/main/java/com/glqdlt/ex/replicationjdbc/DataSourceConfig.java
@@ -5,38 +5,77 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Properties;
 
+@EnableTransactionManagement
 @Configuration
 public class DataSourceConfig {
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em
+                = new LocalContainerEntityManagerFactoryBean();
+        em.setDataSource(lazyConnectionDataSourceProxy());
+        em.setPackagesToScan("com.glqdlt.ex.**");
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        em.setJpaVendorAdapter(vendorAdapter);
+        em.setJpaProperties(additionalProperties());
+
+        return em;
+    }
+    Properties additionalProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.hbm2ddl.auto", "none");
+        properties.setProperty(
+                "hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+
+        return properties;
+    }
+
 
     @Bean
-    public HikariDataSource firstDataSource() {
-        HikariConfig hikariConfig = getHikariConfig("");
+    public PlatformTransactionManager transactionManager(
+            EntityManagerFactory emf){
+        JpaTransactionManager transactionManager = new JpaTransactionManager();
+        transactionManager.setEntityManagerFactory(emf);
+
+        return transactionManager;
+    }
+
+    @Bean
+    public LazyConnectionDataSourceProxy lazyConnectionDataSourceProxy(){
+        return new LazyConnectionDataSourceProxy(firstDataSource());
+    }
+
+    @Bean
+    public DataSource firstDataSource() {
+        HikariConfig hikariConfig = hikariConfigGenerate("jdbc:mysql://127.0.0.1:3306/some");
         return new HikariDataSource(hikariConfig);
     }
 
 
     @Bean
-    public HikariDataSource secondDataSource() {
-        HikariConfig hikariConfig = getHikariConfig("");
+    public DataSource secondDataSource() {
+        HikariConfig hikariConfig = hikariConfigGenerate("jdbc:mysql://127.0.0.1:3306/some2");
         return new HikariDataSource(hikariConfig);
     }
 
-    private HikariConfig getHikariConfig(String url) {
+    private HikariConfig hikariConfigGenerate(String url) {
         HikariConfig hikariConfig = new HikariConfig();
-        hikariConfig.setDriverClassName("");
         hikariConfig.setJdbcUrl(url);
-        hikariConfig.setUsername("");
-        hikariConfig.setPassword("");
+        hikariConfig.setDriverClassName("com.mysql.jdbc.Driver");
+        hikariConfig.setUsername("root");
+        hikariConfig.setPassword("1234");
         return hikariConfig;
-    }
-
-    @Bean
-    public LazyConnectionDataSourceProxy lazyConnectionDataSourceProxy() {
-        LazyConnectionDataSourceProxy lazyConnectionDataSourceProxy = new LazyConnectionDataSourceProxy(firstDataSource());
-        return lazyConnectionDataSourceProxy;
     }
 
 }
--- src/main/java/com/glqdlt/ex/replicationjdbc/ReplicationJdbcApplication.java
+++ src/main/java/com/glqdlt/ex/replicationjdbc/ReplicationJdbcApplication.java
@@ -1,14 +1,61 @@
 package com.glqdlt.ex.replicationjdbc;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+import java.time.LocalDateTime;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+@Slf4j
 @SpringBootApplication
-public class ReplicationJdbcApplication {
+public class ReplicationJdbcApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ReplicationJdbcApplication.class, args);
-	}
+    @Autowired
+    private UserRepo userRepo;
 
+    @Autowired
+    private DataSourceChanger dataSourceChanger;
+
+    @Autowired
+    @Qualifier("secondDataSource")
+    private DataSource secondSource;
+
+
+    @Autowired
+    @Qualifier("firstDataSource")
+    private DataSource firstSource;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReplicationJdbcApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+        executorService.submit(() -> {
+            while (true) {
+                log.info(userRepo.findAll().toString());
+                Thread.sleep(1000);
+            }
+        });
+        executorService.submit(() -> {
+            while (true) {
+                Thread.sleep(2000);
+                int dd = LocalDateTime.now().getSecond();
+                if (dd % 3 == 0) {
+                    dataSourceChanger.changeDataSource(secondSource);
+                }else{
+                    dataSourceChanger.changeDataSource(firstSource);
+                }
+            }
+        });
+    }
 }
 
